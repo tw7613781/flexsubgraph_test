98e74545316801f6882ee508ee1587a87393423b
Tang Wei <tw7613781@gmail.com>
2022-08-17 17:23:24 +0800

    refactor the mapping function

--- src/flex.ts
+++ src/flex.ts
@@ -1,68 +1,30 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  FLEX,
-  Transfer,
-  Approval,
-  OwnershipTransferred
+    FLEX as FLEXContract,
+    Transfer as TransferEvent
 } from "../generated/FLEX/FLEX"
-import { ExampleEntity } from "../generated/schema"
+import { User, TransferHistory } from "../generated/schema"
 
-export function handleTransfer(event: Transfer): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+export function handleTransfer(event: TransferEvent): void {
+    let user = User.load(event.params._to.toHex())
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+    if (!user) {
+        user = new User(event.params._to.toHex())
+        user.address = event.params._to.toHex()
+        user.createdAtTimestamp = event.block.timestamp
+    }
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
+    let flexContract = FLEXContract.bind(event.address)
+    user.balance = flexContract.balanceOf(event.params._to)
+    user.save();
 
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
+    let transferHistory = new TransferHistory(
+        event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+    )
+    transferHistory.blockHeight = event.block.number
+    transferHistory.txHash = event.transaction.hash.toHex()
+    transferHistory.sender = event.params._from.toHex()
+    transferHistory.receiver = event.params._to.toHex()
+    transferHistory.amount = event.params._value
+    transferHistory.save()
 
-  // Entity fields can be set based on event parameters
-  entity._from = event.params._from
-  entity._to = event.params._to
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.decimals(...)
-  // - contract.transfer(...)
-  // - contract.transferFrom(...)
-  // - contract.approve(...)
-  // - contract.increaseAllowance(...)
-  // - contract.decreaseAllowance(...)
-  // - contract.mint(...)
-  // - contract.burnFrom(...)
-  // - contract.transferOwnership(...)
-  // - contract.name(...)
-  // - contract.symbol(...)
-  // - contract.balanceOf(...)
-  // - contract.allowance(...)
-  // - contract.totalSupply(...)
-  // - contract.owner(...)
 }
-
-export function handleApproval(event: Approval): void {}
-
-export function handleOwnershipTransferred(event: OwnershipTransferred): void {}
